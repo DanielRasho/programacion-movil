a7f1cb1dd2c957429e50116c226684aa11aa738f
Daniel Rayo <danirayoroldan@gmail.com>
2023-08-01 09:57:07 -0600

    Fix: Add required functions for calculator

--- Lab2-Calculator/app/src/main/java/uvg/edu/gt/myapplication/Model/ICalculator.kt
+++ Lab2-Calculator/app/src/main/java/uvg/edu/gt/myapplication/Model/ICalculator.kt
@@ -1,4 +1,8 @@
 package uvg.edu.gt.myapplication.Model
 
 interface ICalculator {
+    @Throws(ArithmeticException::class)
+    fun evaluate(expression: String?): Double
+
+    fun getPrevious(): Double
 }
