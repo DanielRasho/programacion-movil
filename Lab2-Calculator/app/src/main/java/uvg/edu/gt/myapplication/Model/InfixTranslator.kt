e57cad9d1789dd17c122eb2f494b98e276a11d93
Jose-Prince <joseprince148@gmail.com>
2023-08-05 19:42:01 -0600

    Feat: Translation to infix to postfix

--- Lab2-Calculator/app/src/main/java/uvg/edu/gt/myapplication/Model/InfixTranslator.kt
+++ Lab2-Calculator/app/src/main/java/uvg/edu/gt/myapplication/Model/InfixTranslator.kt
@@ -2,5 +2,53 @@ package uvg.edu.gt.myapplication.Model;
 
 public class InfixTranslator {
 
-    
+    fun Prec(c : Char) : Int {
+        when (c) {
+            '+' -> return 1;
+            '-' -> return 1;
+
+            '*' -> return 2;
+            '/' -> return 2;
+
+            '^' -> return 3;
+        }
+        return -1;
+    }
+
+    fun infixToPostfix(expression : String) : String {
+        var result : String = "";
+
+        var stack : IStack<Char> = Stack();
+
+        for (c in expression){
+
+            if (Character.isLetterOrDigit(c))
+                result += c;
+
+            else if (c == '(')
+                stack.push(c);
+
+            else if (c == ')'){
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    result += stack.peek();
+                    stack.pop();
+                }
+                stack.pop();
+            } else {
+                while (!stack.isEmpty() && Prec(c) <= Prec(stack.peek())) {
+                    result += stack.peek();
+                    stack.pop();
+                }
+                stack.push(c);
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            if (stack.peek() == '(')
+                return "Expresión inválida";
+            result += stack.peek();
+            stack.pop();
+        }
+        return result;
+    }
 }
